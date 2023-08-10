48d830934b330074c404894d7e9fd99fdeb1c1ae
victordeckmann <141971378+victordeckmann@users.noreply.github.com>
2023-08-10 20:21:22 -0300

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,20 @@
+
+
+/* Includes */
+#include "stm32f4xx.h"
+
+/* Private macro */
+/* Private variables */
+/* Private function prototypes */
+/* Private functions */
+
+
+int main(void)
+{
+
+
+  while (1)
+  {
+
+  }
+}
